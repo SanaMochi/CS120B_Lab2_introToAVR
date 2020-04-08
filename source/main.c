fdc6e55ca273699682f8215bbb8f291f3c47bffa
Sana Mohiuddin <smohi002@wch136-35.cs.ucr.edu>
2020-04-07 22:07:23 -0700

    added build

--- source/main.c
+++ source/main.c
@@ -1,7 +1,7 @@
 /*	Author: smohi002
  *  Partner(s) Name: 
- *	Lab Section:
- *	Assignment: Lab #  Exercise #
+ *	Lab Section: 25
+ *	Assignment: Lab #2  Exercise #0
  *	Exercise Description: [optional - include for your own benefit]
  *
  *	I acknowledge all content contained herein, excluding template or example
@@ -15,9 +15,24 @@
 int main(void) {
     /* Insert DDR and PORT initializations */
 
+	DDRA = 0x00; PORTA = 0xFF; // Configure port A's 8 pins as inputs
+   	DDRB = 0xFF; PORTB = 0x00; // Configure port B's 8 pins as outputs, initialize to 0s
+    	unsigned char tmpB = 0x00; // Temporary variable to hold the value of B
+    	unsigned char tmpA = 0x00; // Temporary variable to hold the value of A
+
+
     /* Insert your solution below */
     while (1) {
-
+	// 1) Read input
+	         tmpA = PINA & 0x01;	
+	if (tmpA == 0x01) { // True if PA0 is 1
+            tmpB = (tmpB & 0xFC) | 0x01; // Sets tmpB to bbbbbb01
+                             // (clear rightmost 2 bits, then set to 01)
+        } else {
+        	tmpB = (tmpB & 0xFC) | 0x02; // Sets tmpB to bbbbbb10
+                             // (clear rightmost 2 bits, then set to 10)
+        }
+	PORTB = tmpB;
     }
-    return 1;
+    return 0;
 }
